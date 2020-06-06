38a66f92076f89e9922a33b61308889287b3150a
Aigul <mardanova065@gmail.com>
2020-06-06 11:12:36 +0300

    r0

---
+++ src/Lab3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Lab3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the x coordinate");
+        double x = in.nextDouble();
+        System.out.println("Enter the y coordinate");
+        double y = in.nextDouble();
+        System.out.println("Enter the radius");
+        double r = in.nextDouble();
+
+
+
+
+        Point point = new Point(x, y, r);
+        Area area = point.getArea();
+
+        if (point.isInArea()) {
+            System.out.println("The point is included in the specified area");
+        }
+        else{System.out.println("The point is not included in the specified area");
+
+        }
+    }
+}
---
+++ src/TestT.java
@@ -0,0 +1,84 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class Test {
+    private static Point zero = new Point(0.0, 0.0, 0.0);
+    private static Point p_1 = new Point(0.0, 0.0, 2.0);
+    private static Point p_2 = new Point(0.0, 0.0, 1.0);
+    private static Point p_3 = new Point(-0.2, 0.1, 1.0);
+    private static Point p_4 = new Point(-1.2, 0.1, 1.0);
+    private static Point p_5 = new Point(1.0, -1.0, 2.0);
+    private static Point p_6 = new Point(1.0, -5.0, 2.0);
+
+    @Test
+    public void testSecondAreaMiss() {
+        assertFalse(p_4.isInArea());
+    }
+	
+	@Test
+    public void testSecondAreaMiss2() {
+        assertFalse(p_4.isInArea());
+    }
+	
+	
+	
+
+    @Test
+    public void testThirdAreaHit() {
+        assertTrue(p_5.isInArea());
+    }
+    @Test
+    public void testFirstAreaHit() {
+        assertTrue(p_1.isInArea());
+    }
+    @Test
+    public void testFirstAreaMiss() {
+        assertTrue(p_2.isInArea());
+    }
+    @Test
+    public void testSecondAreaHit() {
+        assertFalse(p_3.isInArea());
+    }
+    @Test
+    public void testThirdAreaMiss() {
+        assertTrue(p_6.isInArea());
+    }
+    @Test
+    public void testZero() {
+        assertTrue(zero.isInArea());
+    }
+
+
+    @Test
+    public void test1() {
+        assertTrue(new Point(1.0, 2.0, 3.0).isInArea());
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(new Point(-2.0, -1.0, 5.0).isInArea());
+    }
+
+    @Test
+    public void test3() {
+        assertFalse(new Point(3.0, -2.0, 4.0).isInArea());
+    }
+
+    @Test
+    public void test4() {
+        assertFalse(new Point(-5.0, -12.0, 5.0).isInArea());
+    }
+
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testExeption() {
+        Point wrong = new Point(1.0, 1.0, -1.0);
+    }
+    @Test
+    public void testCorrectConstructor() {
+        Point right = new Point(1.0, 1.0, 1.0);
+    }
+    @Test
+    public void testTricky() {
+        Point tricky = new Point(1.0, 1.0, 0.0);
+    }
+}
